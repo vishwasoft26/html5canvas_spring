2f90296ab909c24156bb12bf0f679694c490db9f
Vishwa <vsundarapa@gmail.com>
2020-02-18 22:04:59 -0800

    Initial version

--- src/main/java/com/jira/jiracanvas/rest/JiraCanvasController.java
+++ src/main/java/com/jira/jiracanvas/rest/JiraCanvasController.java
@@ -2,7 +2,6 @@ package com.jira.jiracanvas.rest;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
